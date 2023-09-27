57a50ccf2c4afb2ecf53c8742699e8bf0a51bcd1
DELL <aggalaapoorva4@gmail.com>
2023-09-27 14:47:55 +0530

    demo

---
+++ devops_workshop/src/devops_workshop/devops_workshop.java
@@ -0,0 +1,10 @@
+package devops_workshop;
+
+public class devops_workshop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a devops repository");
+	}
+
+}
